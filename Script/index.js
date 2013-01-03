2b8ac0fc63800b71af8c7b351bea67c3d45346aa
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-01-03 14:53:07 -0800

    Changed to use MapQuest Open Static Map tiles.

--- Script/index.js
+++ Script/index.js
@@ -6,11 +6,11 @@ require(["dojo/string", "esri/map", "esri/layers/WebTiledLayer", "dojo/domReady!
 
 	map = new esri.Map("map");
 
-	mqSubDomains = ["mtile01", "mtile02", "mtile03", "mtile04"];
-	mapQuest = new esri.layers.WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/vx/map/${level}/${col}/${row}.jpg", {
+	mqSubDomains = ["otile1", "otile2", "otile3", "otile4"];
+	mapQuest = new esri.layers.WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/osm/${level}/${col}/${row}.png", {
 		"id": "mapQuest",
 		"subDomains": mqSubDomains,
-		"copyright": "MapQuest © 2012"
+		"copyright": "© MapQuest"
 	});
 
 	map.addLayer(mapQuest);
