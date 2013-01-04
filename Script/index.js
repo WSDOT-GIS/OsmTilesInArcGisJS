cf8d860bb5dea565e59df4ebeda50d37bf220b8a
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-01-03 16:20:52 -0800

    Updated to version 3.3 of the ArcGIS JS API. Corrected attribution for MapQuest / OpenStreetMap.

--- Script/index.js
+++ Script/index.js
@@ -1,17 +1,22 @@
-﻿/*global require*/
-require(["dojo/string", "esri/map", "esri/layers/WebTiledLayer", "dojo/domReady!"], function () {
+﻿/*global require, esri*/
+require(["dojo/on", "esri/map", "esri/layers/WebTiledLayer", "dojo/domReady!"], function (on) {
 	"use strict";
 
-	var map, mqSubDomains, mapQuest;
+	var map, mqSubDomains, mqLayer;
 
 	map = new esri.Map("map");
-
-	mqSubDomains = ["otile1", "otile2", "otile3", "otile4"];
-	mapQuest = new esri.layers.WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/osm/${level}/${col}/${row}.png", {
-		"id": "mapQuest",
-		"subDomains": mqSubDomains,
-		"copyright": "© MapQuest"
+	// dojo.connect(window, 'resize', map, map.resize);
+	on(window, "resize", function () {
+		map.resize();
 	});
 
-	map.addLayer(mapQuest);
+
+	mqSubDomains = ["otile1", "otile2", "otile3", "otile4"];
+	mqLayer = new esri.layers.WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/osm/${level}/${col}/${row}.png", {
+		"id": "mapQuest",
+		"subDomains": mqSubDomains,
+		"copyright": '© OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
+	});
+
+	map.addLayer(mqLayer);
 });
